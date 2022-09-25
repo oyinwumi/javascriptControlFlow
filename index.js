9e9b5843f53b064b984a91ec468d748aa85b9116
Oyindamola Ogunknle <oyinwumi@gmail.com>
2022-09-25 20:45:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let mark = 10;
+if(mark > 90) 
+console.log('AA');
+else if (mark > 80 && mark <= 90)
+console.log('AB');
+else if(mark > 70 && mark <= 80)
+console.logt('BB');
+else if (mark > 60 && mark <= 70)
+console.log('BC');
+ else if (mark > 50 && mark <= 60)
+ console.log('CC');
+else if (mark > 40 && mark <= 50)
+console.log('CD');
+else if (mark > 30 && mark <= 40)
+console.log('DD');
+else {
+    console.log('FF');
+}
